02c6b10ae6996101f404650d14c734b26e4adcb4
Vedha <vyas@gmail.com>
2023-09-16 18:15:35 +0530

    comment.py

---
+++ comment.py
@@ -0,0 +1,6 @@
+a=50  # 1st value declaration 
+b=100 # 2nd value declaration
+c=100 # 3rd value declaration
+#calculation part
+d=a+b+c
+print("the sum of three values are",d)
--- sub.py
+++
@@ -1,5 +0,0 @@
-a=20 
-b=30 
-c=40 
-d=a+b+c 
-print("the sum of three values are ",d)
