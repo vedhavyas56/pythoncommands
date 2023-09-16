65230f2ec1dfa6ef74f2d9afd89aa9cc6a2fb590
Vedha <vyas@gmail.com>
2023-09-16 18:59:00 +0530

    intfloatchar

---
+++ all -arth-function.py
@@ -0,0 +1,16 @@
+a=50 
+b=60 
+c=70 
+d=100 
+
+add=a+d 
+sub=b-c 
+mul=c*a 
+div=d\a 
+mudl=a%d 
+pow=d**a 
+print ("\n the addition of two values",add) 
+print ("\n the substaraction of two values",sub) 
+print ("\n the multiplication of two values",mul) 
+print ("\n the division of two values",div)
+print ("\n the addition of two values",mad)
---
+++ intfloatchar.py
@@ -0,0 +1,6 @@
+a=60 
+b=30.455
+message="hi this is my python message"
+print("\n give value is in integer propgrame format",a)
+print("\n give value is in  float propgrame format",b)
+print("\n given text message in char format: ",message)
