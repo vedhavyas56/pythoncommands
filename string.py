2a47203ca68d73d8756e2bc4ceeb927730d75ac6
Vedha <vyas@gmail.com>
2023-09-16 19:13:10 +0530

    string.py

---
+++ string.py
@@ -0,0 +1,8 @@
+message= 'this is vedhavyas' 
+print("\n",message)
+message= "good morning" 
+print("\n",message)
+message= "this is my laptop"
+print("\n",message)
+message='"where are you.". i am in ananthapur '
+print("\n",message)
