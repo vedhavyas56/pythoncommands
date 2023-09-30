369bbe3752432989efd2dd32de347af5177ff937
Vedha <vyas@gmail.com>
2023-09-30 11:32:03 +0530

    ggh

---
+++ elif-condition.py
@@ -0,0 +1,8 @@
+tommorow= "home"
+
+if tommorow == "warm":
+    print("I'll go to the  beach.")
+elif tommorow == "very hot":
+    print("I'll go to the for swimmimg poll.")
+else:
+    print("I'll  chill in home .")
---
+++ equal.py
@@ -0,0 +1,8 @@
+x=4
+y=5
+if x < y :
+    print ("x is smaller than y")
+elif x  == y :
+    print("x is equal to y")
+else  :
+    print ("x is greater than y")
